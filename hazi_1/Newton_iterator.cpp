df2646a3b78f5d19edcbed92689bf498e4a16a1f
shootingszter <juharoseszter@gmail.com>
2021-03-13 19:38:05 +0100

    start project

--- hazi_1/Newton_iterator.cpp
+++ hazi_1/Newton_iterator.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std; //hogy ne kelljen minden alkalommal kiírni, hogy std ::
+
+//Find the square root of 'num' by starting the iteration from 'x0'
+
+template<typename F, typename T>
+
+T deriv(F f, int n, T x0, T x1)
+{
+   return (f*(x1-x0))/ n;
+}
+
+template<typename F, typename T>
+
+double Newton(F f, T deriv, double x0, double e);
+    {
+    double x1{};
+
+    while(true) 
+    {
+        x1 = x0 - f(x0) / df(x0);
+
+        if(abs( x1 - x0 ) <= e ) break;
+
+        x0 = x1;
+    }
+
+    return x1;
+    }
 
 int main()
 {
-  
+    Newton([](double x){ return x*x-612.0; },
+           [](double x){ return 2.0*x; }, 10.0);
 }
