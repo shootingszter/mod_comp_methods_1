fa95d4c2f358925d1a880dca8160c41ccec8e608
shootingszter <juharoseszter@gmail.com>
2021-03-14 16:15:46 +0100

    correct derivative function

--- hazi_1/Newton_iterator.cpp
+++ hazi_1/Newton_iterator.cpp
@@ -4,25 +4,38 @@
 #include <stdlib.h>
 
 using namespace std; //hogy ne kelljen minden alkalommal kiírni, hogy std ::
-
-//Find the square root of 'num' by starting the iteration from 'x0'
-
-template<typename F, typename T>
+ 
+/*
+deriváló rutin, amiről kiderült, hogy haszontalan
 
 T deriv(F f, int n, T x0, T x1)
 {
    return (f*(x1-x0))/ n;
 }
+*/
+template<typename T>
+
+T function(T x) //a közelíteni kívánt függvény
+{
+    return x*x*x - x*x + 2;
+}
+
+template<typename D>
+
+D dx(D x) //...és az annak deriváltjával visszatérő függvény
+{
+    return 3*x*x - 2*x;
+}
 
 template<typename F, typename T>
 
-double Newton(F f, T deriv, double x0, double e)
+double Newton(F function, T df, double x0, double e)
 {
     double x1{};
 
     while(true) 
     {
-        x1 = x0 - f(x0) / deriv(f, x0, )
+        x1 = x0 - function(x0) / df(x0);
 
         if(abs( x1 - x0 ) <= e ) break;
 
