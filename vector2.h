5453da6840a3dfbdd851f274036fa9ab88d8a926
shootingszter <juharoseszter@gmail.com>
2021-03-25 18:56:27 +0100

    debugged

--- vector2.h
+++ vector2.h
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <math.h>
 
 template<typename T>
 
 struct Vector2
 {
     T x, y;
+};
 
-    Vector2<T>& operator+=( Vector2<T> const& v)
+template<typename T>
+
+Vector2<T>& operator+=( Vector2<T> const& v)
     {
         x += y.x; y += v.y;
         return *this;
     }
 
-    Vector2<T>& operator+=( Vector2<T> const& a, Vector2<T> const& b )
+template<typename T>
+
+Vector2<T>& operator+=( Vector2<T> const& a, Vector2<T> const& b )
     {
         return Vector2<T>{a.x + b.x, a.y + b.y};
     }
-};
+
+int main()
+{
+    
+}
