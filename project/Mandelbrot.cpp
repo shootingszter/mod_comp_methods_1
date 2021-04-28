a49b29849560d075fd00832417f657c1fb6a4cd1
shootingszter <juharoseszter@gmail.com>
2021-04-28 13:24:07 +0200

    declare mandelbrot

--- project/Mandelbrot.cpp
+++ project/Mandelbrot.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 #include <ostream>
 #include <cmath>
+#include <complex>
 
+template<typename T>
 
+auto Mandelbrot(T c, T max_iter)
+{
+    int z = c;
+
+    for (int i = 0; i < max_iter; i++)
+    {
+        if (abs(z) > 2)
+        {
+            return i;
+        }
+            
+        z = z*z + c;
+    } 
+
+    return 0;
+}
