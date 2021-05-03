fa1af45d997bcfdb9357215819b2ff8223f9ff97
shootingszter <juharoseszter@gmail.com>
2021-05-03 11:56:55 +0200

    initialize input

--- project/Mandelbrot.cpp
+++ project/Mandelbrot.cpp
@@ -1,23 +1,38 @@
-#include <iostream>
 #include <ostream>
-#include <cmath>
-#include <complex>
+#include <iostream>
+#include <fstream>
 
-template<typename T>
-
-auto Mandelbrot(T c, T max_iter)
+/*
+while (x*x + y*y < 2*2 AND iteration < max_iteration)
 {
-    int z = c;
+    xtemp = x*x - y*y +x0
+        y = 2 * x*y + y0
+        x = xtemp
+        iteration = iteration + 1
+}*/
 
-    for (int i = 0; i < max_iter; i++)
+int main()
+{
+    //get the required input values from file
+
+    std::ifstream fin("input.txt");
+    int imageWidth, imageHeight, maxN;
+    double minR, maxR, minI, maxI;
+    if (!fin)
     {
-        if (abs(z) > 2)
-        {
-            return i;
-        }
-            
-        z = z*z + c;
-    } 
+        std::cout << "COuld not open file!" << std::endl;
+        std::cin.ignore();
+        return 0;
+    }
 
-    return 0;
+    fin >> imageWidth >> imageHeight;
+    //open the output file, write the header
+
+    // for every pixel...
+    //...find the real and imaginary values for c, corresponding to that x, y pixel in the image
+    //...find the number of iterations in the mandekbrot formula using that c
+    //...map the resulting number to an rgb value
+    //...output it to the image
+
+
 }
