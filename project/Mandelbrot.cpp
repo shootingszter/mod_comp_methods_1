879713cd343cf8f53737269a2d0b9b211a426bb0
shootingszter <juharoseszter@gmail.com>
2021-05-05 21:31:15 +0200

    mission completed

--- project/Mandelbrot.cpp
+++ project/Mandelbrot.cpp
@@ -2,20 +2,21 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
-
+//including the header 
 #include "Mandelheader.h"
 
 
 
 int main()
 {
-
+    //declare used paramteres
     int imageWidth = 1000;
     int imageHeight = 1000;
     int maxN = 1000;
 
     double minR, maxR, minI, maxI;
 
+    //these ar the input prameteres that can be changed by the user
     std::cout << "Enter minReal:\n";
     std::cin >> minR;
     
@@ -31,28 +32,29 @@ int main()
     //open the output file, write the header
     std::ofstream fout("Mandelplot.ppm");
 
-    fout << "P3" << std::endl;
-    fout << imageWidth << " " << imageHeight << std::endl;
-    fout << "256" << std::endl;
+    fout << "P3" << std::endl; //generate an RGB color image
+    fout << imageWidth << " " << imageHeight << std::endl; //size
+    fout << "256" << std::endl; //the maximum value for each color
 
     // for every pixel...
     for (int y = 0; y < imageHeight; y++) //rows
     {
-        for (int x = 0; x < imageWidth; x++)
+        for (int x = 0; x < imageWidth; x++) //cols
         {
-            //...find the real and imaginary values for c, corresponding to that x, y pixel in the image
+            //find the real and imaginary values for c, corresponding to that x, y pixel in the image
             double cr = mapToReal<double>(x, imageWidth, minR, maxR);
             double ci = mapToImaginary<double>(y, imageHeight, minI, maxI);
 
-            //...find the number of iterations in the mandekbrot formula using that c
+            //find the number of iterations in the mandelbrot formula using that c
             int n = findMandelbrot<int>(cr, ci, maxN);
-            //...map the resulting number to an rgb value
+
+            //map the resulting number to an rgb value
+            //this results in an orange- yellow- pink- green palette
             int r = (n * n % 256); 
             int g = (n * n % 100);
             int b = (n % 119);
 
-            //...output it to the image
-
+            //output it to the image
             fout << r << " " << g << " " << b << " ";
         }
 
@@ -60,7 +62,8 @@ int main()
     }
     
     fout.close();
-    std::cout << "Finished!" << std::endl;
+    std::cout << "Finished!" << std::endl; //sign that the output image is done
+    
     return 0;
 
 }
--- project/Mandelheader.h
+++ project/Mandelheader.h
@@ -3,18 +3,18 @@
 #include <fstream>
 #include <math.h>
 
-
+//returns with the real value that's gonna be represented by the pixel
 template <typename I, typename D>
 
 D mapToReal(I x, I imageWidth, D minR, D maxR)
 {
     double range = maxR - minR;
 
-    return x*(range/imageWidth) + minR;
+    return x*(range/imageWidth) + minR; 
 
 }
 
-
+//the same function just for y 
 template <typename I, typename D>
 
 double mapToImaginary(I y, I imageHeight, D minI, D maxI)
@@ -25,14 +25,28 @@ double mapToImaginary(I y, I imageHeight, D minI, D maxI)
 }
 
 
+/*
+My guideline for the finMandelbrot was the following algorithm:
+
+while(x*x + y*y < 2*2 AND iteration < max_ieration)
+{
+    xtemp = x*x - y*y + x0
+    y = 2*x*y +y0
+    x = xtemp
+
+    iteration = iteration + 1
+}
+*/
+
 template <typename I, typename D>
 
 int findMandelbrot(D cr, D ci, I max_iterations)
 {
     int i = 0;
-    double zr = 0.0, zi = 0.0;
+    double zr = 0.0, zi = 0.0; //real and imaginary components of the complex number
 
-    while (i < max_iterations && zr * zr + zi * zi < 4.0)
+    //the iteration with a while loop, using the escape conditions
+    while (i < max_iterations && zr * zr + zi * zi < 4.0) 
     {
         double temp = zr * zr - zi * zi + cr;
 
@@ -42,6 +56,6 @@ int findMandelbrot(D cr, D ci, I max_iterations)
         i++;
     }
 
-    return i;
+    return i; //return the number of iteration 
 
 }
